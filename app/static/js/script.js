92cf8e51a58fd01651e8c4dff83b2f74af651189
Konstantin-create <schahov.konst@gmail.com>
2023-11-25 22:14:39 +0300

    feat: working on form

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -24,3 +24,26 @@ function formatPhoneNumber(phoneNumber) {
 
     return formattedPhoneNumber;
 }
+
+
+var price_number = document.getElementById('price_number');
+
+price_number.addEventListener('input', function (e) {
+    e.target.value = formatPriceNumber(e.target.value);
+});
+
+function formatPriceNumber(price_number) {
+    if (price_number.length % 4 === 0 && price_number >= 3) {
+        return price_number + '.'
+    }
+    return price_number
+}
+
+function formatInputNumber() {
+    let input = document.getElementById('price_number')
+    let value = input.value
+    if (value.length > 3) {
+        input.value = input.value + '.'
+    }
+    console.log(value)
+}
